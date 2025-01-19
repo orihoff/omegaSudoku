3bc424d7f73d8d0bc4bd62386f31894a3235c5d8
orihoff <ori.hoffnung@gmail.com>
2025-01-19 21:05:43 +0200

    deleting unusful exceptions classes

--- SudokuExceptions.cs
+++ SudokuExceptions.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HoffSudoku
+﻿namespace omegaSudoku.Exceptions
 {
-    internal class SudokuExceptions
+    // Base class for all custom Sudoku exceptions
+    public class SudokuException : Exception
     {
+        public SudokuException(string message) : base(message) { }
+    }
+
+    // Exception for invalid input
+    public class InvalidInputException : SudokuException
+    {
+        public InvalidInputException(string message) : base(message) { }
+    }
+
+    // Exception for invalid Sudoku board
+    public class InvalidBoardException : SudokuException
+    {
+        public InvalidBoardException(string message) : base(message) { }
+    }
+
+    // Exception for initialization errors
+    public class InitializationException : SudokuException
+    {
+        public InitializationException(string message) : base(message) { }
+    }
+
+    // Exception for failed puzzle solving
+    public class SolveFailureException : SudokuException
+    {
+        public SolveFailureException(string message) : base(message) { }
+    }
+
+    // General runtime exception for unexpected errors
+    public class RuntimeException : SudokuException
+    {
+        public RuntimeException(string message) : base(message) { }
     }
 }
